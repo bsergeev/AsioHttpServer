322f815fe83cf901649a4a4b1544e2271055bbcb
Boris Sergeev <bsergeev@gmail.com>
2017-09-17 19:15:15 -0600

    Support passing build configuration to Bash build script

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 BUILD_DIRECTORY=build
-
+BUILD_CONFIGURATION=Debug
+if [ "$1" = "Release" ]; then
+  BUILD_CONFIGURATION="$1"
+fi
 if [ ! -d "$BUILD_DIRECTORY" ]; then
   mkdir build
 fi
 
 cd $BUILD_DIRECTORY
-cmake -G "Unix Makefiles" ..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$BUILD_CONFIGURATION" ..
 make
 cd ..
 
